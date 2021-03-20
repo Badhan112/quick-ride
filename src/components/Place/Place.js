52d54133741f660093976d4808c0397f790a2c29
Badhan112 <badhancsesastc112@gmail.com>
2021-03-20 17:09:28 +0600

    pick from & to place implemented

--- src/components/Place/Place.js
+++ src/components/Place/Place.js
@@ -1,13 +1,25 @@
 import { faPlaneArrival, faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useContext, useState } from 'react';
-import { Col, Container } from 'react-bootstrap';
+import React, { useContext, useEffect, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { VehicleContext } from '../../App';
 import './Place.css';
+import mockData from '../../data/mockData.json';
+import AvailableDriver from '../AvailableDriver/AvailableDriver';
 
 const Place = () => {
     const [place, setPlace] = useState({});
     const [selectedVehicle] = useContext(VehicleContext);
+    const [availableDrivers, setAvailableDrivers] = useState([]);
+
+    useEffect(() => {
+        if (selectedVehicle) {
+            const drivers = mockData.filter(data => data.vehicle_type === selectedVehicle);
+            setAvailableDrivers(drivers);
+        } else {
+            setAvailableDrivers(mockData);
+        }
+    }, [selectedVehicle]);
 
     const selectedPlace = {
         pickFrom: '',
@@ -19,29 +31,41 @@ const Place = () => {
     }
 
     const handlePlaceForm = event => {
-        console.log(selectedPlace);
         setPlace(selectedPlace);
         event.preventDefault();
     }
 
     return (
         <Col sm={12} md={5} lg={4}>
-            <Container className="place-area">
-                <form onSubmit={handlePlaceForm}>
-                    <label htmlFor="pickFrom">Pick From</label>
-                    <input type="text" name="pickFrom" onBlur={handlePlaceInput} className="place-input-field" />
-                    <label htmlFor="pickTo">Pick To</label>
-                    <input type="text" name="pickTo" onBlur={handlePlaceInput} className="place-input-field" />
-                    <input type="submit" value="Search" className="submit-button" />
-                </form>
-            </Container>
-            <Container className="place-area">
-                <div className="selected-place-area">
-                    <h4><FontAwesomeIcon icon={faPlaneDeparture} className="place-icon" /> {place.pickFrom}</h4>
-                    <h4><FontAwesomeIcon icon={faPlaneArrival} className="place-icon" /> {place.pickTo}</h4>
-                    <h4>{selectedVehicle}</h4>
-                </div>
-            </Container>
+            {
+                !place.pickFrom && !place.pickTo
+
+                    ? <Container className="place-area">
+                        <form onSubmit={handlePlaceForm}>
+                            <label htmlFor="pickFrom">Pick From</label>
+                            <input type="text" name="pickFrom" onBlur={handlePlaceInput} className="place-input-field" />
+                            <label htmlFor="pickTo">Pick To</label>
+                            <input type="text" name="pickTo" onBlur={handlePlaceInput} className="place-input-field" />
+                            <input type="submit" value="Search" className="submit-button" />
+                        </form>
+                    </Container>
+
+                    : <Container className="place-area">
+                        <div className="selected-place-area">
+                            <h4><FontAwesomeIcon icon={faPlaneDeparture} className="place-icon" /> {place.pickFrom}</h4>
+                            <h4><FontAwesomeIcon icon={faPlaneArrival} className="place-icon" /> {place.pickTo}</h4>
+                        </div>
+                        <Container>
+                            <Row>
+                                {
+                                    availableDrivers.map(driver => <AvailableDriver data={driver} />)
+                                }
+                            </Row>
+                        </Container>
+                    </Container>
+            }
+
+
         </Col>
     );
 };
